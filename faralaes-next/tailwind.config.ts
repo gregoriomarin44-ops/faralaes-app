7ad7765ba95e109c018f7a389e0a22e3170def2d
GregorioMarin <gregoriomarin@yahoo.es>
2026-05-03 20:37:15 +0200

    Fix Tailwind paths

--- faralaes-next/tailwind.config.ts
+++ faralaes-next/tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 CLAVE
+  ],
   theme: {
     extend: {
       colors: {
